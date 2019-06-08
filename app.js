bf1be17091e318606b8d4a7dc9c153d80796eab5
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-06-08 19:38:31 -0400

    updated files with basic npm variables

---
+++ app.js
@@ -0,0 +1,7 @@
+var axios = require("axios");
+var nodeSpotifyAPI = require("Node-Spotify-API");
+var spotify = new nodeSpotifyAPI(keys.spotify);
+var moment = require("moment")
+var DotEnv = require("DotEnv").config()
+var keys = require("./keys.js");
+
