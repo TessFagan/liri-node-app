07ed141801a5e8fe921e3e8272a975ee4c4bb450
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-06-08 22:18:14 -0400

    spotify api and movie api updates

--- keys.js
+++ keys.js
@@ -2,13 +2,7 @@
 
 console.log('this is loaded');
 
-exports.spotify = {
+exports.Spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-
-
-// Spotfiy project name: HarvardBootcamp
-// Client ID: 01c3764aa7fc4bf18aa32486776e41d7
-// Client Secret: 1fab54ef42474d84a8e26b3e187adcdb
--- liri.js
+++ liri.js
@@ -1,10 +1,10 @@
 
+require("DotEnv").config()
 var axios = require("axios");
-// var Spotify = require('node-spotify-api');
-// var spotifyKeys = new Spotify(keys.Spotify);
-var moment = require("moment")
-var dotEnv = require("DotEnv").config()
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
+var spotifyKeys = new Spotify(keys.Spotify);
+var moment = require("moment")
 
 
 // Make it so liri.js can take in one of the following commands:
@@ -62,19 +62,78 @@ function concertThis(input) {
 
 
 function spotifyThis() {
+  spotifyKeys.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err + `"The Sign by Ace of Base"`);
+
+    }
+    console.log(data.tracks.items);
+    var songs = data.tracks.items
+    songs.forEach(Object => {
+      let artistName = Object.artists[0].name
+      let songName = Object.name
+      let previewLink = Object.preview_url
+      let album = Object.album.name
+
+      console.log(`\n${artistName}`)
+      console.log(previewlink)
+      console.log(album)
+    })
+
+  });
+
+
+  // Artist(s)
+
+  // * The song's name
+
+  // * A preview link of the song from Spotify
+
+  // * The album that the song is from
+
+
 
 }
 
-function movieThis() {
+function movieThis(input) {
+
+  var queryUrl = `http://www.omdbapi.com/?apikey=trilogy&t=${input}`
+
+  axios.get(queryUrl)
+    .then(function (response) {
+      var movie = response.data
+      let movieName = movie.Title
+      let movieYear = movie.Year
+      let movieIMDB = movie.imdbRating
+      let movieRotten = movie.Ratings[1].Value
+      let movieCountry = movie.Country
+
+
+      console.log(`\n${movieName}`)
+      console.log(movieYear)
+      console.log(movieIMDB)
+    })
+
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range
+        console.log(error.response.data);
+      }
+    })
+}
+
+
+  // Title of the movie.
+  // * Year the movie came out.
+  // * IMDB Rating of the movie.
+  // * Rotten Tomatoes Rating of the movie.
+  // * Country where the movie was produced.
+  // * Language of the movie.
+  // * Plot of the movie.
+  // * Actors in the movie.
 
 }
 
 function doWhatItSays() {
 
 }
-
-
-// concert.forEach(Element => {
-//   let venueName = Element.venue.name
-//           let venueLocation = `${element.venue.country} + ${element.venue.region}+ ${element.venue.city}`
-//           console.log(venueLocation)})
